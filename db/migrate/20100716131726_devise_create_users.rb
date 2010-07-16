32c02f79a975314a3ed22edc93f953013832045e
Erik Nyhus <eri-nyh@hotmail.com>
2010-07-16 15:19:30 +0200

    Removed confirmable and confirmation token from users migration

--- db/migrate/20100716131726_devise_create_users.rb
+++ db/migrate/20100716131726_devise_create_users.rb
@@ -2,7 +2,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
   def self.up
     create_table(:users) do |t|
       t.database_authenticatable :null => false
-      t.confirmable
       t.recoverable
       t.rememberable
       t.trackable
@@ -12,7 +11,6 @@ class DeviseCreateUsers < ActiveRecord::Migration
     end
 
     add_index :users, :email,                :unique => true
-    add_index :users, :confirmation_token,   :unique => true
     add_index :users, :reset_password_token, :unique => true
     # add_index :users, :unlock_token,         :unique => true
   end
