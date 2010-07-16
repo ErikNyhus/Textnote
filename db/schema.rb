ff89e7189375e35dacc139db7ac116a455e4057f
Erik Nyhus <eri-nyh@hotmail.com>
2010-07-16 18:55:48 +0200

    Added author to posts and migrated

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20100716131726) do
+ActiveRecord::Schema.define(:version => 20100716164812) do
+
+  create_table "posts", :force => true do |t|
+    t.string   "title"
+    t.text     "body"
+    t.string   "author"
+    t.boolean  "published"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "email",                               :default => "", :null => false
